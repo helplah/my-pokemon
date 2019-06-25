b4738ff54e4c4ae037c0b2983d1c7c195afff551
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-25 21:29:36 +0800

    Shift tests in app.test.js to pokemon.test.js

--- test/pokemon.test.js
+++ test/pokemon.test.js
@@ -1,7 +1,7 @@
-const pokemon = require("../pokemon");
+const controller = require("../pokemon.controller");
 const { MongoClient } = require("mongodb");
 
-describe("insert", () => {
+describe("Pokemon", () => {
   let connection;
   let db;
 
@@ -22,27 +22,38 @@ describe("insert", () => {
     await db.close();
   });
 
-  it("insertOne and findOne should return one pokemon", async () => {
-    const pikachu = {
-      _id: "5d0b3dd4da9a863b78902c8b",
-      id: 25,
-      name: { english: "Pikachu", japanese: "ピカチュウ", chinese: "皮卡丘" },
-      type: ["Electric"],
-      base: {
-        HP: 35,
-        Attack: 9999,
-        Defense: 40,
-        SpAttack: 50,
-        SpDefence: 50,
-        Speed: 90
-      }
-    };
+  describe("/pokemon", () => {
+    it("/GET should return all pokemon from database", () => {});
 
-    await pokemon.insertOne(pikachu);
-    const pokemonCollection = db.collection("pokemons");
-    const mockPokemon = { id: 25 };
+    it("/POST should create a pokemon in database", () => {});
+  });
 
-    const foundPokemon = await pokemonCollection.findOne(mockPokemon);
-    expect(foundPokemon.name.english).toEqual("Pikachu");
+  describe("/pokemon/:id", () => {
+    it("/GET should return a pokemon", async () => {
+      const pikachu = {
+        id: 25,
+        name: { english: "Pikachu", japanese: "ピカチュウ", chinese: "皮卡丘" },
+        type: ["Electric"],
+        base: {
+          HP: 35,
+          Attack: 9999,
+          Defense: 40,
+          SpAttack: 50,
+          SpDefence: 50,
+          Speed: 90
+        }
+      };
+
+      await controller.insertOne(pikachu);
+      const pokemonCollection = db.collection("pokemons");
+      const mockPokemon = { id: 25 };
+
+      const foundPokemon = await pokemonCollection.findOne(mockPokemon);
+      expect(foundPokemon.name.english).toEqual("Pikachu");
+    });
+
+    it("/PUT should modify a pokemon from database", () => {});
+
+    it("/DELETE should remove a pokemon from database", () => {});
   });
 });
