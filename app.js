b258decd8323b5e285d37f9dacbd389530a9ad0b
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-29 13:46:12 +0800

    Update app and server

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
-const app = express();
 const pokemonRouter = require("./routes/pokemonRouter");
 
+const app = express();
+
 app.use(express.json());
+app.get("/", (req, res) => res.send("Hello world"));
 app.use("/pokemon", pokemonRouter);
 
 app.use((err, res, req, next) => {
--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// where should I put db
-// require("./db");
+require("./db");
 
 const app = require("./app");
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Port ${port} running!`);
