f013796ef54c2239c0bffd24face916f25755a4c
Jenssen Lee <30688276+helplah@users.noreply.github.com>
2019-06-29 13:00:09 +0800

    Add data folder for pokemon data

---
+++ data/pokemon.data.js
@@ -0,0 +1,88 @@
+module.exports = [
+  {
+    name: {
+      english: "Squirtle",
+      japanese: "ゼニガメ",
+      chinese: "杰尼龟"
+    },
+    base: {
+      HP: 44,
+      Attack: 48,
+      Defense: 65,
+      SpAttack: 50,
+      SpDefence: 64,
+      Speed: 43
+    },
+    type: ["Water"],
+    _id: "5cfe022f42b1786675d67e79",
+    id: 7
+  },
+  {
+    name: {
+      english: "Charizard",
+      japanese: "リザードン",
+      chinese: "喷火龙"
+    },
+    base: {
+      HP: 78,
+      Attack: 84,
+      Defense: 78,
+      SpAttack: 109,
+      SpDefence: 85,
+      Speed: 100
+    },
+    type: ["Fire", "Flying"],
+    _id: "5cfe022f42b1786675d67e7b",
+    id: 6
+  },
+  {
+    name: {
+      english: "Caterpie",
+      japanese: "キャタピー",
+      chinese: "绿毛虫"
+    },
+    base: {
+      HP: 45,
+      Attack: 30,
+      Defense: 35,
+      SpAttack: 20,
+      SpDefence: 20,
+      Speed: 45
+    },
+    type: ["Bug"],
+    _id: "5cfe022f42b1786675d67e7d",
+    id: 10
+  },
+  {
+    name: {
+      english: "Metapod",
+      japanese: "トランセル",
+      chinese: "铁甲蛹"
+    },
+    base: {
+      HP: 50,
+      Attack: 20,
+      Defense: 55,
+      SpAttack: 25,
+      SpDefence: 25,
+      Speed: 30
+    },
+    type: ["Bug"],
+    _id: "5cfe022f42b1786675d67e7e",
+    id: 11
+  },
+  {
+    name: { english: "Pikachu", japanese: "ピカチュウ", chinese: "皮卡丘" },
+    base: {
+      HP: 35,
+      Attack: 9999,
+      Defense: 40,
+      SpAttack: 50,
+      SpDefence: 50,
+      Speed: 90
+    },
+    type: ["Electric"],
+    _id: "5d0b3dd4da9a863b78902c8b",
+    id: 25
+  }
+];
